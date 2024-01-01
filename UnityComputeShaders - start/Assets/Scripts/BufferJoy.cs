bbbbbdd3cb1712595ebd8a4b403165b67784001f
SkyWalker2506 <musabkara1990@gmail.com>
2024-01-02 02:16:45 +0300

    lesson 4 ended

--- UnityComputeShaders - start/Assets/Scripts/BufferJoy.cs
+++ UnityComputeShaders - start/Assets/Scripts/BufferJoy.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class BufferJoy : MonoBehaviour
 {
@@ -17,8 +16,9 @@ public class BufferJoy : MonoBehaviour
     public Color circleColor = new Color();
 
     int count = 10;
-
-    // Use this for initialization
+    Circle[] circleData;
+    ComputeBuffer circleBuffer;
+    
     void Start()
     {
         outputTexture = new RenderTexture(texResolution, texResolution, 0);
@@ -36,6 +36,30 @@ public class BufferJoy : MonoBehaviour
     private void InitData()
     {
         circlesHandle = shader.FindKernel("Circles");
+        
+        uint threadGroupSizeX;
+        shader.GetKernelThreadGroupSizes(circlesHandle, out threadGroupSizeX, out _, out _);
+        
+        int totalThreads = (int)threadGroupSizeX * count;
+        
+        circleData = new Circle[totalThreads];
+        
+        float speed = 100;
+        float halfSpeed = speed * 0.5f;
+        float minRadius = 10;   
+        float maxRadius = 30;
+        float radiusRange = maxRadius - minRadius;
+
+        for (int i = 0; i < totalThreads; i++)
+        {
+            Circle circle = circleData[i];
+            circle.center.x = Random.value* texResolution;
+            circle.center.y = Random.value* texResolution;
+            circle.velocity.x = Random.value * speed - halfSpeed;
+            circle.velocity.y = Random.value * speed - halfSpeed;
+            circle.radius = Random.value * radiusRange + minRadius;
+            circleData[i] = circle;
+        }
     }
 
     private void InitShader()
@@ -48,7 +72,10 @@ public class BufferJoy : MonoBehaviour
 		
 		shader.SetTexture( clearHandle, "Result", outputTexture );
         shader.SetTexture( circlesHandle, "Result", outputTexture );
-
+        int stride = (2+2+1)*sizeof(float);
+        circleBuffer = new ComputeBuffer(circleData.Length, stride);
+        circleBuffer.SetData(circleData);
+        shader.SetBuffer(circlesHandle, "circlesBuffer", circleBuffer);
         rend.material.SetTexture("_MainTex", outputTexture);
     }
  
@@ -62,6 +89,19 @@ public class BufferJoy : MonoBehaviour
     void Update()
     {
         DispatchKernels(count);
+    }
+
+    void OnDestroy()
+    {
+        circleBuffer.Release();
+    }
+    
+    struct Circle
+    {
+        public Vector2 center;
+        public Vector2 velocity;
+        public float radius;
+    
     }
 }
 
