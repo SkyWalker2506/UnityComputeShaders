e8ddfa0ae652dd093f3b92d91d0163c2955ab509
SkyWalker2506 <musabkara1990@gmail.com>
2024-01-01 23:24:20 +0300

    lesson 2 ended

--- UnityComputeShaders - start/Assets/Scripts/SolidColor.cs
+++ UnityComputeShaders - start/Assets/Scripts/SolidColor.cs
@@ -3,50 +3,43 @@ using System.Collections;
 
 public class SolidColor : MonoBehaviour
 {
+    [SerializeField] private ComputeShader shader;
+    [SerializeField] private int textResolution = 256;
+    [SerializeField] private string kernelName="SolidRed";
+    
+    private Renderer rend;
+    private RenderTexture outputTexture;
+    private int kernelHandle;
+    private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+    private static readonly int Result = Shader.PropertyToID("Result");
+    private static readonly int TextResolution = Shader.PropertyToID("texResolution");
 
-    public ComputeShader shader;
-    public int texResolution = 256;
-
-    Renderer rend;
-    RenderTexture outputTexture;
-
-    int kernelHandle;
-
-    // Use this for initialization
-    void Start()
+    private void Start()
     {
-        outputTexture = new RenderTexture(texResolution, texResolution, 0);
-        outputTexture.enableRandomWrite = true;
+        outputTexture = new RenderTexture(textResolution, textResolution, 0)
+        {
+            enableRandomWrite = true
+        };
         outputTexture.Create();
-
         rend = GetComponent<Renderer>();
         rend.enabled = true;
-
+        rend.material.mainTexture = outputTexture;
         InitShader();
     }
 
     private void InitShader()
     {
-        kernelHandle = shader.FindKernel("CSMain");
-
-        shader.SetTexture(kernelHandle, "Result", outputTexture);
- 
-        rend.material.SetTexture("_MainTex", outputTexture);
-
-        DispatchShader(texResolution / 8, texResolution / 8);
+        kernelHandle = shader.FindKernel(kernelName);
+        shader.SetInt(TextResolution, textResolution);
+        shader.SetTexture(kernelHandle, Result, outputTexture);
+        rend.material.SetTexture(MainTex, outputTexture);
+        DispatchShader(textResolution / 8, textResolution / 8);
     }
 
     private void DispatchShader(int x, int y)
     {
         shader.Dispatch(kernelHandle, x, y, 1);
     }
-
-    void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.U))
-        {
-            DispatchShader(texResolution / 8, texResolution / 8);
-        }
-    }
+  
 }
 
