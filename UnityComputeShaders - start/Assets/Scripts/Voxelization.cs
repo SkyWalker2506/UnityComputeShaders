57214d65f570087d63478fb0549dcb6e2d942703
SkyWalker2506 <musabkara1990@gmail.com>
2024-01-17 02:00:33 +0300

    Lesson 19 Ended

--- UnityComputeShaders - start/Assets/Scripts/Voxelization.cs
+++ UnityComputeShaders - start/Assets/Scripts/Voxelization.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Voxelization : MonoBehaviour
 {
@@ -8,6 +6,17 @@ public class Voxelization : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        VoxelizeMesh vm = GetComponent<VoxelizeMesh>();
+        vm.Voxelize(vm.meshToVoxelize);
+        float ps = vm.ParticleSize;
+        Vector3 scale = new Vector3(ps, ps, ps);
         
+        foreach(Vector3 pos in vm.PositionList)
+        {
+            GameObject particle = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            particle.transform.position = pos;
+            particle.transform.localScale = scale;
+            particle.transform.parent = transform;
+        }
     }
 }
--- UnityComputeShaders - start/Assets/Scripts/VoxelizeMesh.cs
+++ UnityComputeShaders - start/Assets/Scripts/VoxelizeMesh.cs
@@ -28,6 +28,67 @@ public class VoxelizeMesh : MonoBehaviour
 
     public void Voxelize(Mesh mesh)
     {
+        GameObject go = new GameObject("Voxelized Mesh");
+        go.layer = layer;
+        MeshCollider mc = go.AddComponent<MeshCollider>();
+        mc.sharedMesh = mesh;
+        Vector3 minExtents = mc.bounds.center - mc.bounds.extents;
+        Vector3 maxExtents = mc.bounds.center + mc.bounds.extents;
+        RaycastHit hit;
+        float radius = mesh.bounds.extents.y / yParticleCount;
+        particleSize = radius * 2;
+        Vector3 rayOffset = minExtents;
+        Vector3 counts = mesh.bounds.extents / radius;
+        Vector3Int particleCounts = new Vector3Int((int)counts.x, (int)counts.y, (int)counts.z);
         
+        if(particleCounts.x % 2 == 0)
+        {
+            minExtents.x+=(mesh.bounds.extents.x - particleCounts.x * radius);
+        }
+        float offsetZ = 0;
+        if(particleCounts.z % 2 == 0)
+        {
+            offsetZ+=(mesh.bounds.extents.z - particleCounts.z * radius);
+        }
+
+        rayOffset.y += radius;
+        
+        int layerMask = 1 << layer;
+        while (rayOffset.y < maxExtents.y)
+        {
+            rayOffset.x = minExtents.x;
+            while (rayOffset.x < maxExtents.x)
+            {
+                Vector3 rayOrigin = go.transform.position + rayOffset;
+                
+                if(Physics.Raycast(rayOrigin, Vector3.forward, out hit, 100, layerMask))
+                {
+                    Vector3 frontPt = hit.point;
+                    rayOrigin.z += maxExtents.z * 2;
+                    if(Physics.Raycast(rayOrigin, Vector3.back, out hit, 100, layerMask))
+                    {
+                        Vector3 backPt = hit.point;
+                        int n = Mathf.CeilToInt(frontPt.z / ParticleSize);
+                        frontPt.z = n * ParticleSize;
+                        while (frontPt.z < backPt.z)
+                        {
+                            float gap = backPt.z - frontPt.z;
+                            if(gap < radius*.5f)
+                            {
+                                break;  
+                            }
+                            
+                            positions.Add(frontPt);
+                            frontPt.z += ParticleSize;
+                        }
+                    }
+                }
+
+                rayOffset.x += particleSize;
+            }
+            rayOffset.y += particleSize;
+
+        }
+
     }
 }
